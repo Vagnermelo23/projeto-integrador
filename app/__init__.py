3b3aa8c11be2d4a954bfac41409581e7de021a23
Vagner Ferreira de Melo <165937363+Vagnermelo23@users.noreply.github.com>
2024-05-12 01:01:33 -0300

    flash ,dados mysql

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "sua_chave_secreta_aqui"
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
 from app import app
 import mysql.connector
 import os 
@@ -8,35 +8,44 @@ mydb = mysql.connector.connect(
     user=os.environ.get('DB_USER', 'root'),
     port=os.environ.get('DB_PORT', 3306),
     password=os.environ.get('DB_PASSWORD', ''),
-    database=os.environ.get('DB_DATABASE', 'bd')
+    database=os.environ.get('DB_DATABASE', 'db')
 )
 
-
 @app.route('/')
-def default ():
+def default():
     return render_template('index.html')
 
 @app.route('/index')
-def index ():
+def index():
     return render_template('index.html')
 
 @app.route('/idoso')
-def idoso ():
+def idoso():
     return render_template('idoso.html')
 
-
 @app.route('/gestante')
-def gestante ():
+def gestante():
     return render_template('gestante.html')
 
 @app.route('/recemnascido')
-def recemnascido ():
+def recemnascido():
     return render_template('recemnascido.html')
 
-@app.route('/autenticar3')
-def autenticar3 ():
+@app.route('/autenticar3', methods=['POST'])
+def autenticar3():
     nome = request.form.get('nome')
     datanc = request.form.get('datanc')
-    mf = request.form.get('M','F')
+    sexo = request.form.get('sexo')
     peso = request.form.get('peso')
 
+    cursor = mydb.cursor()
+    try:
+        cursor.execute('INSERT INTO cadastro (nome, datanc, mf, peso) VALUES (%s, %s, %s, %s)',
+                       (nome, datanc, sexo, peso))
+        mydb.commit()
+        cursor.close()
+        flash("Cadastro confirmado", "success")
+        return redirect('/recemnascido')
+    except mysql.connector.Error as err:
+        flash(f"Erro ao cadastrar: {err}", "error")
+        return redirect('/recemnascido')
